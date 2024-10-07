2487bb5ecdab577aa382ba1145a989b957a14596
Mirzajalilov Ravshanbek <mirzajalilovravshanbek@gmail.com>
2024-10-07 17:23:40 +0500

    port changed

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ var app = require('express')()
 var http = require('http').Server(app)
 require('./startup/routes')(app);
 require('./startup/prod')(app);
-http.listen(3000, () => {
-  console.log('Listening on *:3000')
+http.listen(3030, () => {
+  console.log('Listening on *:3030')
 })
